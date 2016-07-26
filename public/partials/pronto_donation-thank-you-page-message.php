a0cc2a5b1c46c4bf9f95700145efd896a7926e1c
junjiecanonio <junjie@alphasys.com.au>
2016-07-26 15:14:54 +0800

    updated pronto donation settings again

---
+++ public/partials/pronto_donation-thank-you-page-message.php
@@ -0,0 +1,11 @@
+	<br/>
+	<br/>
+	<?php
+
+	$pronto_donation_settings = (empty(get_option('pronto_donation_settings'))) ? "" : get_option('pronto_donation_settings');
+	$thank_you_page_message_page = (empty($pronto_donation_settings['ThankYouPageMessagePage'])) ? "" : $pronto_donation_settings['ThankYouPageMessagePage'];
+
+	$my_postid = $thank_you_page_message_page;//This is page id or post id
+	$content_post = get_post($my_postid);
+	$content = $content_post->post_content;
+	echo $content;
