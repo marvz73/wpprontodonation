37653e2f600324ea7914fba4ce716abb74d8b563
junjiecanonio <junjie@alphasys.com.au>
2016-07-26 18:06:03 +0800

    updated pronto donation settings again

--- admin/partials/pronto_donation-admin-display.php
+++ admin/partials/pronto_donation-admin-display.php
@@ -434,7 +434,7 @@ if ( isset($_GET['page']) ) {
 		
 		//================ Get Post Page for Messages ==============//
 		$thank_you_page_message_post_id = '';
-		$thank_you_page_message_postTitle = 'pronto_donation_thank_you_page_message';
+		$thank_you_page_message_postTitle = 'pronto donation thank you page message';
 		$post_id_A = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . $thank_you_page_message_postTitle . "'" );
 
 	    if(empty($post_id_A)||$post_id_A==null){}
@@ -443,7 +443,7 @@ if ( isset($_GET['page']) ) {
 	    }
 
 	    $info_on_offline_payment_panel_post_id = '';
-		$info_on_offline_payment_panel_postTitle = 'pronto_donation_info_on_offline_payment_panel';
+		$info_on_offline_payment_panel_postTitle = 'pronto donation info on offline payment panel';
 		$post_id_B = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . $info_on_offline_payment_panel_postTitle . "'" );
 
 		if (empty($post_id_B)||$post_id_B==null) {}
@@ -452,7 +452,7 @@ if ( isset($_GET['page']) ) {
 	    }
 
 	   	$instructions_emailed_to_offline_donor_before_payment_post_id = '';
-		$instructions_emailed_to_offline_donor_before_payment_postTitle = 'pronto_donation_instructions_emailed_to_offline_donor_before_payment';
+		$instructions_emailed_to_offline_donor_before_payment_postTitle = 'pronto donation instructions emailed to offline donor before payment';
 		$post_id_C = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . $instructions_emailed_to_offline_donor_before_payment_postTitle . "'" );
 
 		if (empty($post_id_C)||$post_id_C==null) {}
