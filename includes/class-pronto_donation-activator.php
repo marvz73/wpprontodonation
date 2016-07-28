7bc0225baa57c6c7830fd7cd0b2c3c253fd1258c
junjiecanonio <junjie@alphasys.com.au>
2016-07-28 13:19:49 +0800

    updated pronto donation settings again

--- includes/class-pronto_donation-activator.php
+++ includes/class-pronto_donation-activator.php
@@ -197,6 +197,12 @@ class Pronto_donation_Activator {
 		);
 		$pronto_donation_settings = (empty(get_option('pronto_donation_settings'))) ? "" : get_option('pronto_donation_settings');
 
+		$args = array(
+	      'public' => true,
+	      'label'  => 'pronto_donation'
+	    );
+	    register_post_type( 'pronto_donation', $args );	
+
 
 		$thank_you_page_message_post_id = '';
 		$thank_you_page_message_postTitle = 'pronto donation thank you page message';
@@ -208,7 +214,7 @@ class Pronto_donation_Activator {
 	            'post_status' => 'publish',
 	            'post_date' => date('Y-m-d H:i:s'),
 	            'post_author' => '',
-	            'post_type' => 'staff',
+	            'post_type' => 'pronto_donation',
 	            'post_category' => array(0)
 	        );
 			$thank_you_page_message_post_id = wp_insert_post($new_post);
@@ -226,7 +232,7 @@ class Pronto_donation_Activator {
 	            'post_status' => 'publish',
 	            'post_date' => date('Y-m-d H:i:s'),
 	            'post_author' => '',
-	            'post_type' => 'staff',
+	            'post_type' => 'pronto_donation',
 	            'post_category' => array(0)
 	        );
 			$cancel_page_message_post_id = wp_insert_post($new_post);
