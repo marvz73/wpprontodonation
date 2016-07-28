273b50d6d01fe37570e552abccd43a752cd43251
marvin <marvin@alphasys.com.au>
2016-07-28 13:14:24 +0800

    eway payment gateway, set currency code on campaign transaction

--- public/class-pronto_donation-public.php
+++ public/class-pronto_donation-public.php
@@ -113,7 +113,7 @@ class Pronto_donation_Public {
 	// Author: Marvin Aya-ay
 	private $base = __DIR__ . '/../payments/';
 	public function pronto_donation_campaign( $campaign_id ) {
-
+		$option = get_option('pronto_donation_settings');
 		//Process the payment here...
 	    if($_POST)
 	    {
@@ -121,10 +121,12 @@ class Pronto_donation_Public {
 	    	if($campaign_data['action'] == 'process_donate' && wp_verify_nonce( $campaign_data['nonce'], 'donation'))
 	    	{
 			
-				$option = get_option('pronto_donation_settings');
-
+			 	// global $wpdb;
+				// $wpdb->query("UPDATE $wpdb->postmeta SET meta_value = '123123123123' WHERE meta_id = 28");
+	    	
 	    		$campaign_data['status'] = 'pending';
-	    		
+	    		$campaign_data['CurrencyCode'] = $option['CurrencyCode'];
+
 	    		$payment_methods = $this->class->pronto_donation_payment_methods();
 
 	    		foreach($payment_methods as $index=>$payment)
@@ -137,13 +139,12 @@ class Pronto_donation_Public {
 
 	    		$campaign_data['redirectURL'] = get_home_url() . '/?p=' . $option['ThankYouPageMessagePage'] . '&payment_gateway=' . $campaign_data['payment'];
 
+	    		$campaign_data['CancelUrl']   = get_home_url() . '/?p=' . $option['CancelPageMessagePage']. '&payment_status=C&ref=' . $campaign_data['post_meta_id'];
+
   				$post_meta_id = add_post_meta($campaign_data['donation_campaign'], 'pronto_donation_donor', $campaign_data);
 
   				$campaign_data['post_meta_id'] = $post_meta_id;
 
-			 	// global $wpdb;
-				// $wpdb->query("UPDATE $wpdb->postmeta SET meta_value = '123123123123' WHERE meta_id = 28");
-	    		
 	    		// Call the payment function to execute payment action
 	    		$campaign_data['payment_info']->payment_process($campaign_data);
 
@@ -151,6 +152,13 @@ class Pronto_donation_Public {
 	    }
 	    else
 	    {
+
+	    	$campaignOption = new stdClass();
+			foreach ($option as $key => $value)
+			{
+			    $campaignOption->$key = $value;
+			}
+
 		    //Display the donation fields
 			$attrs = shortcode_atts( array(
 		        'campaign' => 0,
@@ -166,39 +174,46 @@ class Pronto_donation_Public {
 		    $pronto_donation_user_info = get_post_meta($attrs['campaign'], 'pronto_donation_user_info', true);
 
 		    require_once('partials/pronto_donation-public-campaign.php');
+
 	    }
 	}
 
 	public function pronto_donation_override_template($page_template){
 
-		if (get_the_ID() == get_option('pronto_donation_settings')['ThankYouPageMessagePage'])
+		if (isset($_GET['payment_gateway']) && get_the_ID() == get_option('pronto_donation_settings')['ThankYouPageMessagePage'])
 		{
     		global $wpdb;
+
     		$payment_methods = $this->class->pronto_donation_payment_methods();
-			$donor = $wpdb->get_results("SELECT * FROM $wpdb->postmeta WHERE meta_id = " . esc_html($_GET['PaymentReference']));
-			$campaign = maybe_unserialize($donor[0]->meta_value);
-			
-			if(empty($campaign['payment_response']) && !array_key_exists('payment_response', $campaign)){
 
-	    		foreach($payment_methods as $index=>$payment)
-	    		{
-	    			
-	    			if(strtolower($_GET['payment_gateway']) == strtolower($payment->payment['payment_name']))
-	    			{
-	    				//call payment process complete
-	    				$payment->payment_complete($campaign, $_GET);
+    		foreach($payment_methods as $index=>$payment)
+    		{
+    			
+    			if(strtolower($_GET['payment_gateway']) == strtolower($payment->payment['payment_name']))
+    			{
+    				//call payment process complete
+    				$payment->payment_complete($_GET);
 
-						// Salesforce syncing will be call here...
+					// SALESFORCE LOGIC SYNC HERE...
 
-	    			}
-	    		}
+    			}
     		}
 		} //Cancel Transaction
-		else if (isset($_GET['PaymentReference']) && $_GET['PaymentReference'] == 'C' && get_the_ID() == get_option('pronto_donation_settings')['CancelPageMessagePage'])
+		else if ($_GET['payment_status'] == 'C' && get_the_ID() == get_option('pronto_donation_settings')['CancelPageMessagePage'])
 		{	
-			echo 'Cancel Transaction...';
+
+			$campaign_id = esc_html($_GET['ref']);
+
+			$campaignDonor = $wpdb->get_results("SELECT * FROM $wpdb->postmeta WHERE meta_id = " . $campaign_id);
+
+			$campaign = maybe_unserialize($campaignDonor[0]->meta_value);
+			if($campaign['status'] != 'CANCELLED')
+			{
+				$campaign['status'] = 'CANCELLED';
+				$wpdb->query("UPDATE $wpdb->postmeta SET meta_value = '".(maybe_serialize($campaign))."' WHERE meta_id = " . $campaign_id);
+			}
 		}
-		
+
 	}
 
 	public function pronto_donation_thank_you_page_message(){
--- public/partials/pronto_donation-public-campaign.php
+++ public/partials/pronto_donation-public-campaign.php
@@ -1,6 +1,5 @@
 <!-- <form method="post" action="<?php echo home_url( '/wp-admin/admin-post.php' ) ?>"> -->
-<form method="post" >
-
+<form method="post" class="<?php echo $campaignOption->FormClass ?>" >
 
 	<!-- Donor Information -->
 	<fieldset>
@@ -8,7 +7,7 @@
 
 		<?php if($this->class->pronto_donation_has_payment_amount_level($attrs['campaign'])): ?>
 			<p>
-				<label>Donation Amount: </label>
+				<label>Donation Amount </label>
 
 				<?php
 					$this->class->pronto_donation_payment_amount_level($attrs['campaign']);
@@ -23,23 +22,23 @@
 
 		if(!$pronto_donation_campaign['hide_custom_amount']): ?>
 		<p>
-			<label>Donation Custom Amount: </label>
+			<label>Donation Custom Amount </label>
 			<?php if($this->class->pronto_donation_has_payment_amount_level($attrs['campaign'])): ?>
-				<span id="currency">
+				<span id="currency" class="<?php echo $campaignOption->InputFieldClass ?>">
 					<span><?php echo $this->class->pronto_donation_currency(); ?></span>
-					<input disabled="" type="number" id="pd_custom_amount" name="pd_custom_amount" placeholder="0.00"step="0.01"  />
+					<input  disabled="" type="number" id="pd_custom_amount" name="pd_custom_amount" placeholder="00" />
 				</span>
 			<?php else: ?>
-				<span id="currency">
+				<span id="currency" class="<?php echo $campaignOption->InputFieldClass ?>">
 					<span><?php echo $this->class->pronto_donation_currency(); ?></span>
-					<input type="number" id="pd_custom_amount" name="pd_custom_amount" placeholder="0.00" step="0.01" />
+					<input type="number" id="pd_custom_amount" name="pd_custom_amount" placeholder="00"  />
 				</span>
 			<?php endif; ?>
 		</p>
 		<?php endif; ?>
 		
 		<p>
-			<label>Donation Type :</label>
+			<label>Donation Type</label>
 			<?php if($pronto_donation_campaign['donation_type'] == 'both'): ?>	
 					<label>
 						<input type="radio" name="donation_type" value="single" checked="true" /> Single
@@ -69,7 +68,7 @@
 		<?php if($pronto_donation_user_info['user_donor_type_option'] != 'hide'): ?>
 			<p>
 				<label>Donor Type</label>
-				<select id="donorType" name="donor_type" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_donor_type_option']) ?> >
+				<select id="donorType" class="<?php echo $campaignOption->InputFieldClass ?>" name="donor_type" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_donor_type_option']) ?> >
 					<option value="I">Individual</option>
 					<option value="B">Business</option>
 				</select>
@@ -77,7 +76,7 @@
 
 			<p id="companyName" style="display: none;">
 				<label>Company Name</label> 
-				<input name="companyName" type="text" />	
+				<input class="<?php echo $campaignOption->InputFieldClass ?>" name="companyName" type="text" />	
 			</p>
 
 		<?php endif; ?>
@@ -85,42 +84,42 @@
 		<?php if($pronto_donation_user_info['user_email_option'] != 'hide'): ?>
 		<p>
 			<label>Email</label>
-			<input name="email" type="email" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_email_option']) ?>/>
+			<input name="email" class="<?php echo $campaignOption->InputFieldClass ?>" type="email" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_email_option']) ?>/>
 		</p>
 		<?php endif; ?>
 
 		<?php if($pronto_donation_user_info['user_firstname_option'] != 'hide'): ?>
 		<p>
 			<label>First Name</label>
-			<input name="first_name" type="text" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_firstname_option']) ?>/>
+			<input name="first_name" class="<?php echo $campaignOption->InputFieldClass ?>" type="text" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_firstname_option']) ?>/>
 		</p>
 		<?php endif; ?>
 
 		<?php if($pronto_donation_user_info['user_lastname_option'] != 'hide'): ?>
 		<p>
 			<label>Last Name</label>
-			<input name="last_name" type="text" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_lastname_option']) ?>/>
+			<input name="last_name" class="<?php echo $campaignOption->InputFieldClass ?>" type="text" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_lastname_option']) ?>/>
 		</p>
 		<?php endif; ?>
 
 		<?php if($pronto_donation_user_info['user_phone_option'] != 'hide'): ?>
 		<p>
 			<label>Phone</label>
-			<input name="phone" type="text" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_phone_option']) ?>/>
+			<input name="phone" class="<?php echo $campaignOption->InputFieldClass ?>" type="text" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_phone_option']) ?>/>
 		</p>
 		<?php endif; ?>
 
 		<?php if($pronto_donation_user_info['user_address_option'] != 'hide'): ?>
 		<p>
 			<label>Address</label>
-			<input name="address" type="text" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_address_option']) ?>/>
+			<input name="address" class="<?php echo $campaignOption->InputFieldClass ?>" type="text" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_address_option']) ?>/>
 		</p>
 		<?php endif; ?>
 
 		<?php if($pronto_donation_user_info['user_country_option'] != 'hide'): ?>
 		<p>
 			<label>Country</label>
-			<select id="country" name="country" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_country_option']) ?>>
+			<select id="country" class="<?php echo $campaignOption->InputFieldClass ?>" name="country" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_country_option']) ?>>
 				<option>Select</option>
 			</select>
 		</p>
@@ -129,7 +128,7 @@
 		<?php if($pronto_donation_user_info['user_state_option'] != 'hide'): ?>
 		<p>
 			<label>State</label>
-			<select id="state"  name="state" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_state_option']) ?>>
+			<select id="state"  class="<?php echo $campaignOption->InputFieldClass ?>" name="state" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_state_option']) ?>>
 				<option>Select</option>
 			</select>
 		</p>
@@ -138,14 +137,14 @@
 		<?php if($pronto_donation_user_info['user_postcode_option'] != 'hide'): ?>
 		<p>
 			<label>Post Code</label>
-			<input name="post_code" type="text" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_postcode_option']) ?>/>
+			<input name="post_code" class="<?php echo $campaignOption->InputFieldClass ?>" type="text" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_postcode_option']) ?>/>
 		</p>
 		<?php endif; ?>
 
 		<?php if($pronto_donation_user_info['user_suburb_option'] != 'hide'): ?>
 		<p>
 			<label>Suburb</label>
-			<input name="suburb" type="text" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_suburb_option']) ?>/>
+			<input name="suburb" class="<?php echo $campaignOption->InputFieldClass ?>" type="text" <?php $this->class->pronto_donation_is_required($pronto_donation_user_info['user_suburb_option']) ?>/>
 		</p>
 		<?php endif; ?>
 
@@ -183,7 +182,7 @@
 	<input type="hidden" name="action" value="process_donate"/>
 	<br>
 	<p class="submit">
-		<button class="button button-primary">Donate</button>
+		<button class="button button-primary <?php echo $campaignOption->ButtonClass ?>"> <?php echo ($campaignOption->EditButtonCaption) ? $campaignOption->EditButtonCaption : 'Donate' ?> </button>
 	</p>
 
 </form>
