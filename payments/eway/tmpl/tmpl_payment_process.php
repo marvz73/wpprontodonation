18e5890f6ec43bf005507ef1dd34c12919f8bc22
marvin <marvin@alphasys.com.au>
2016-07-28 16:31:01 +0800

    update for eway redirection

--- payments/eway/tmpl/tmpl_payment_process.php
+++ payments/eway/tmpl/tmpl_payment_process.php
@@ -2,14 +2,10 @@
 <script type="text/javascript">
 	
 	setTimeout(function(){
-		// window.location.href = "<?php echo $result->SharedPaymentUrl ?>";
+		window.location.href = "<?php echo $result->SharedPaymentUrl ?>";
 	}, 200)
 
 </script>
-
-<!-- <form method="post" action="<?php echo $result->SharedPaymentUrl ?>">
-	<button type="submit">Pay Now</button>
-</form> -->
 
 <div style="text-align: center;">
 	<div class="circle-wrapper">
