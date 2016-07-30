c36c067039223720153cf53d4e4300cf9208d57e
Danryl <danryl12344@gmail.com>
2016-07-30 16:18:54 +0800

    Pushing Something

--- admin/partials/pronto_donation-donation-thickbox.php
+++ admin/partials/pronto_donation-donation-thickbox.php
@@ -176,10 +176,10 @@
                         </th>
                         <td>
                             <select class="regular-text donation-details-value" name="donation_status" id="donation_status">
-                                <option value="pending" <?php if( array_key_exists('statusText', $donation_details ) && strtolower( $donation_details['statusText'] ) == 'pending'  && $donation_details['statusCode'] != 1 ) echo "selected='selected'"; ?> >Pending</option>
+                                <option value="pending" <?php if( array_key_exists('statusText', $donation_details ) && $donation_details['statusCode'] != 1 ) echo "selected='selected'"; ?> >Pending</option>
                                 <option value="cancelled" <?php if( (array_key_exists('statusText', $donation_details) && strtolower( $donation_details['statusText'] ) == 'cancelled' && $donation_details['statusCode'] != 1 ) || 
                                     ( $donation_details['payment'] == 'eWay' && strtolower($donation_details['status'] ) ) === 'cancelled') echo "selected='selected'"; ?> >Cancelled</option>
-                                <option value="approved" <?php if( array_key_exists('statusText', $donation_details) && strtolower( $donation_details['statusText'] ) == 'approved' && $donation_details['statusCode'] == 1 ) echo "selected='selected'"; ?> >Approved</option>
+                                <option value="approved" <?php if( array_key_exists('statusText', $donation_details) && $donation_details['statusCode'] == 1 ) echo "selected='selected'"; ?> >Approved</option>
                             </select>
                             <span class="status-ajax"></span>
                         </td>
