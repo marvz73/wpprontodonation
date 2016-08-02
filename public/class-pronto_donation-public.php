1d9a69dda99aefca2750831e9b86d76997f1c2cc
marvin <marvin@alphasys.com.au>
2016-08-02 17:05:02 +0800

    update statusText init on public class

--- public/class-pronto_donation-public.php
+++ public/class-pronto_donation-public.php
@@ -188,7 +188,7 @@ class Pronto_donation_Public {
 	    		}
 
 	    		$campaign['statusCode'] = 0;
-
+	    		$campaign['statusText'] = '';
 	    		$campaign_data['timestamp'] = time();
 
 	    		$campaign_data['redirectURL'] = get_home_url() . '/?p=' . $this->campaignOption->ThankYouPageMessagePage . '&payment_gateway=' . $campaign_data['payment'];
