33051d46979aa3717adf27dcae15831d62f3055b
Danryl <danryl12344@gmail.com>
2016-08-17 14:00:17 +0800

    fixies for recaptcha visibility

--- payments/ezidebit/tmpl/tml_self_payment_process.php
+++ payments/ezidebit/tmpl/tml_self_payment_process.php
@@ -266,7 +266,7 @@
 	 			} else if( $(this).val() == 'eWay' ) { // when user select eway
 
 	 				$('.self-payment-style').hide();
-	 				$('.self-payment-msg').empty();x
+	 				$('.self-payment-msg').empty();
 	 				$('.g-recaptcha').show();
 
 	 				$('#payNowButton').removeAttr('disabled');
