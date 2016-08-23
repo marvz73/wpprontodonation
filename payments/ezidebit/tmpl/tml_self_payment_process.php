779f7163b42b8fb82eac26662848299052e8a4ef
Danryl <danryl12344@gmail.com>
2016-08-23 20:55:31 +0800

    Fixing error messages on ezidebit payment gateway

--- payments/ezidebit/tmpl/tml_self_payment_process.php
+++ payments/ezidebit/tmpl/tml_self_payment_process.php
@@ -119,6 +119,90 @@
 
 		// payment process function for ezidebit 
 
+		function get_ezidebit_response( errcode ) {
+			var responses = {
+				'01' : "Refer to Card Issuer",
+				'02' : "Refer to Issuer’s Special Conditions",
+				'03' : "Invalid Merchant",
+				'04' : "Pick Up Card",
+				'05' : "Do Not Honour",
+				'06' : "Error",
+				'07' : "Pick Up Card, Special Conditions",
+				'09' : "Request in Progress",
+				'12' : "Invalid Transaction",
+				'13' : "Invalid Amount",
+				'14' : "Invalid Card Number",
+				'15' : "No Such Issuer",
+				'17' : "Customer Cancellation",
+				'18' : "Customer Dispute",
+				'19' : "Re-enter Transaction",
+				'20' : "Invalid Response",
+				'21' : "No Action Taken",
+				'22' : "Suspected Malfunction",
+				'23' : "Unacceptable Transaction Fee",
+				'24' : "File Update not Supported by Receiver",
+				'25' : "Unable to Locate Record on File",
+				'26' : "Duplicate File Update Record",
+				'27' : "File Update Field Edit Error",
+				'28' : "File Update File Locked Out",
+				'29' : "File Update not Successful",
+				'30' : "Format Error",
+				'31' : "Bank not Supported by Switch",
+				'32' : "Completed Partially",
+				'33' : "Expired Card-Pick Up",
+				'34' : "Suspected Fraud-Pick Up",
+				'35' : "Contact Acquirer-Pick Up",
+				'36' : "Restricted Card-Pick Up",
+				'37' : "Call Acquirer Security-Pick Up",
+				'38' : "Allowable PIN Tries Exceeded",
+				'39' : "No CREDIT Account",
+				'40' : "Requested Function not Supported",
+				'41' : "Lost Card-Pick Up",
+				'42' : "No Universal Amount",
+				'43' : "Stolen Card-Pick Up",
+				'44' : "No Investment Account",
+				'51' : "Insufficient Funds",
+				'52' : "No Cheque Account",
+				'53' : "No Savings Account",
+				'54' : "Expired Card",
+				'55' : "Incorrect PIN",
+				'56' : "No Card Record",
+				'57' : "Trans. not Permitted to Cardholder",
+				'58' : "Transaction not Permitted to Terminal",
+				'59' : "Suspected Fraud",
+				'60' : "Card Acceptor Contact Acquirer",
+				'61' : "Exceeds Withdrawal Amount Limits",
+				'62' : "Restricted Card",
+				'63' : "Security Violation",
+				'64' : "Original Amount Incorrect",
+				'65' : "Exceeds Withdrawal Frequency Limit",
+				'66' : "Card Acceptor Call Acquirer Security",
+				'67' : "Hard Capture-Pick Up Card at ATM",
+				'68' : "Response Received Too Late",
+				'75' : "Allowable PIN Tries Exceeded",
+				'86' : "ATM Malfunction",
+				'87' : "No Envelope Inserted",
+				'88' : "Unable to Dispense",
+				'89' : "Administration Error",
+				'90' : "Cut-off in Progress",
+				'91' : "Issuer or Switch is Inoperative",
+				'92' : "Financial Institution not Found",
+				'93' : "Trans Cannot be Completed",
+				'94' : "Duplicate Transmission",
+				'95' : "Reconcile Error",
+				'96' : "System Malfunction",
+				'97' : "Reconciliation Totals Reset",
+				'98' : "MAC Error",
+				'99' : "Reserved for National Use"
+			};
+
+			if( errcode in responses ) {
+				return responses[errcode];
+			} else {
+				return null;
+			}
+		}
+
 		var your_a_robot = 0;
 		var captcha_id = null;
 		var captcha_response = null;
@@ -250,43 +334,56 @@
 							// this function will be the success callback for ezidebit client side
 							your_a_robot = 0;
 							var displaySubmitCallback = function(data) {
- 
-								setTimeout(function(){
-									if(your_a_robot == 0) {
+ 								console.log('response data', data)
+ 								var success_response = ['00', '08', '10', '11', '16', '77', '000', '003'];
+ 								if( $.inArray( data.PaymentResultCode, success_response ) != -1 ) {
+ 									// console.log('hahahaah success')
+									setTimeout(function(){
+										if(your_a_robot == 0) {
 
-										var campaign_id = '<?php echo $ajax_campaign_id ?>';
-										var selected_donation_type = $('input[name=donation_type]:checked').val();
-										
-										// this ajax request will be the captcha validation
-										$.ajax({
-											type: 'POST',
-											url:  ajax_frontend.ajax_url,
-											data: { 'action':'ezi_self_payment_proccess', 'data' : formData, 'campaign_id' : campaign_id, 'ezidebit_api_response' : data },
-											success: function(response){
+											var campaign_id = '<?php echo $ajax_campaign_id ?>';
+											var selected_donation_type = $('input[name=donation_type]:checked').val();
+											
+											// this ajax request will be the captcha validation
+											$.ajax({
+												type: 'POST',
+												url:  ajax_frontend.ajax_url,
+												data: { 'action':'ezi_self_payment_proccess', 'data' : formData, 'campaign_id' : campaign_id, 'ezidebit_api_response' : data },
+												success: function(response){
 
-												// console.log(response)
-												// console.log(data)
+													// console.log(response)
+													// console.log(data)
 
-												var additional_url = '';
+													var additional_url = '';
 
-												for(var k in data) {
-													additional_url += '&'+ k + '=' + data[k];
+													for(var k in data) {
+														additional_url += '&'+ k + '=' + data[k];
+													}
+
+													if( typeof response.success != 'undefined' && response.success ) {
+														window.location.href = response.data.redirect_url + $.trim(additional_url) + '&DonationMetaID=' + response.data.donation_meta_id;
+													}
+													your_a_robot = 0;
+												},
+												error: function(xhr, textStatus, errorThrown) {
+
+													$('.self-payment-msg').append('<p class="ezidebit-error"> Something went wrong, Please try again </p>');
+													$('.ezi-lazy-loading').hide();
+													return;
 												}
-
-												if( typeof response.success != 'undefined' && response.success ) {
-													window.location.href = response.data.redirect_url + $.trim(additional_url) + '&DonationMetaID=' + response.data.donation_meta_id;
-												}
-												your_a_robot = 0;
-											},
-											error: function(xhr, textStatus, errorThrown) {
-
-												$('.self-payment-msg').append('<p class="ezidebit-error"> Something went wrong, Please try again </p>');
-												$('.ezi-lazy-loading').hide();
-												return;
-											}
-										});
-									}
-								}, 3000);
+											});
+										}
+									}, 3000);
+ 								} else {
+ 									var response_txt = get_ezidebit_response( data.PaymentResultCode );
+ 									if( response_txt != null ) {
+ 										$('.self-payment-msg').append('<p class="ezidebit-error"> '+response_txt+' </p>');
+ 									} else {
+ 										$('.self-payment-msg').append('<p class="ezidebit-error"> '+data.PaymentResultText+' </p>');
+ 									}
+ 									$('.ezi-lazy-loading').hide();
+ 									return;
+ 								}
 							};
 
 							// this function will be the error callback for ezidebit client side
